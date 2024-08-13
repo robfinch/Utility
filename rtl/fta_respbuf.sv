038e9b9451213814aa40ef36bc56f2b26e241e6e
Robert Finch <5563401+robfinch@users.noreply.github.com>
2024-08-12 20:59:09 -0400

    Update fta_respbuf.sv

--- rtl/fta_respbuf.sv
+++ rtl/fta_respbuf.sv
@@ -1,7 +1,7 @@
 `timescale 1ns / 10ps
 // ============================================================================
 //        __
-//   \\__/ o\    (C) 2023  Robert Finch, Waterloo
+//   \\__/ o\    (C) 2023-2024  Robert Finch, Waterloo
 //    \  __ /    All rights reserved.
 //     \/_//     robfinch<remove>@finitron.ca
 //       ||
@@ -36,8 +36,9 @@
 
 import fta_bus_pkg::*;
 
-module fta_respbuf(rst, clk, resp, resp_o);
+module fta_respbuf128(rst, clk, resp, resp_o);
 parameter CHANNELS = 8;
+parameter WIDTH = 128;
 localparam HBIT = $clog2(CHANNELS);
 input rst;
 input clk;
@@ -46,22 +47,42 @@ output fta_cmd_response128_t resp_o;
 
 fta_cmd_response128_t [CHANNELS-1:0] respbuf [0:3];
 
-reg [HBIT:0] tmp;
-reg [1:0] tmp2;
+reg [4:0] pri;
+reg [HBIT:0] tmp, tndx;
+reg [1:0] tmp2, t2ndx;
 reg [1:0] chcnt [0:CHANNELS-1];
 
 integer nn1, nn2, nn3, nn4;
+integer crr;	// channel response ready.
 
-// Search for channel with response ready.
 always_comb
 begin
+	if (CHANNELS != 8 && CHANNELS != 4 && CHANNELS != 2) begin
+		$display("fta_respbuf: CHANNELS should be one of 2, 4 or 8");
+		$finish;
+	end
+end
+
+// Search for channel with response ready. The search starts with a different
+// channel each clock cycle. The start channel for the search increments so that
+// channels placed on the output bus come from input in a circular fashion. This
+// is to prevent one channel from hogging the bus. In a similar fashion which
+// buffer is checked first rotates.
+always_comb
+begin
+	crr = 1'd0;
 	tmp2 = 'd0;
-	tmp = {HBIT+1{1'b1}};
+	pri = 5'h1F;
+	tmp = {HBIT{1'b1}};
 	for (nn1 = 0; nn1 < CHANNELS; nn1 = nn1 + 1) begin
 		for (nn3 = 0; nn3 < 4; nn3 = nn3 + 1) begin
-			if (respbuf[nn3][nn1].ack && tmp[HBIT]) begin
-				tmp = nn1;
-				tmp2 = nn3;
+			if (respbuf[(nn1+tndx)%CHANNELS][(nn3+t2ndx)%4].ack) begin
+				if ($signed({1'b0,respbuf[(nn1+tndx)%CHANNELS][(nn3+t2ndx)%4].pri}) > $signed(pri)) begin
+					tmp = nn1;
+					tmp2 = nn3;
+					pri = {1'b0,respbuf[(nn1+tndx)%CHANNELS][(nn3+t2ndx)%4].pri};
+					crr = 1'b1;
+				end
 			end
 		end
 	end
@@ -75,25 +96,27 @@ if (rst) begin
 		for (nn4 = 0; nn4 < 4; nn4 = nn4 + 1)
 			respbuf[nn4][nn2] <= 'd0;
 	end
+	{tndx,t2ndx} <= 'd0;
 end
 else begin
+	tndx <= tndx + 2'd1;
+	if (tndx[HBIT-1:0]=='d0)
+		t2ndx <= t2ndx + 2'd1;
 	resp_o.ack <= 1'b0;
 	resp_o.err <= fta_bus_pkg::OKAY;
 	resp_o.rty <= 'd0;
 	resp_o.dat <= 'd0;
 	resp_o.tid <= 'd0;
-	resp_o.cid <= 'd0;
 	resp_o.adr <= 'd0;
 	resp_o.stall <= 'd0;
 	resp_o.next <= 'd0;
 	resp_o.pri <= 4'hF;
-	if (!tmp[HBIT]) begin
+	if (crr) begin
 		respbuf[tmp2][tmp[HBIT-1:0]].ack <= 1'b0;
 		resp_o.ack <= 1'b1;
 		resp_o.err <= respbuf[tmp2][tmp[HBIT-1:0]].err;
 		resp_o.rty <= respbuf[tmp2][tmp[HBIT-1:0]].rty;
 		resp_o.dat <= respbuf[tmp2][tmp[HBIT-1:0]].dat;
-		resp_o.cid <= respbuf[tmp2][tmp[HBIT-1:0]].cid;
 		resp_o.tid <= respbuf[tmp2][tmp[HBIT-1:0]].tid;
 		resp_o.adr <= respbuf[tmp2][tmp[HBIT-1:0]].adr;
 		resp_o.pri <= respbuf[tmp2][tmp[HBIT-1:0]].pri;
@@ -117,19 +140,38 @@ input clk;
 input fta_cmd_response64_t [CHANNELS-1:0] resp;
 output fta_cmd_response64_t resp_o;
 
-fta_cmd_response64_t [CHANNELS-1:0] respbuf;
+fta_cmd_response64_t [CHANNELS-1:0] respbuf [0:3];
 
-reg [HBIT:0] tmp;
+reg [4:0] pri;
+reg [HBIT:0] tmp, tndx;
+reg [1:0] tmp2, t2ndx;
+reg [1:0] chcnt [0:CHANNELS-1];
+integer nn1, nn2, nn3;
+integer crr;
 
-integer nn1, nn2;
+always_comb
+	if (CHANNELS != 8 && CHANNELS != 4 && CHANNELS != 2) begin
+		$display("fta_respbuf128: CHANNELS should be one of 2,4, or 8");
+		$finish;
+	end
 
 // Search for channel with response ready.
 always_comb
 begin
-	tmp <= {HBIT+1{1'b1}};
+	crr = 1'b0;
+	tmp2 = 'd0;
+	tmp = {HBIT+1{1'b1}};
+	pri = 5'h1F;
 	for (nn1 = 0; nn1 < CHANNELS; nn1 = nn1 + 1) begin
-		if (respbuf[nn1].ack) begin
-			tmp <= nn1;
+		for (nn3 = 0; nn3 < 4; nn3 = nn3 + 1) begin
+			if (respbuf[(nn1+tndx)%CHANNELS][(nn3+t2ndx)%4].ack) begin
+				if ($signed({1'b0,respbuf[(nn1+tndx)%CHANNELS][(nn3+t2ndx)%4].pri}) > $signed(pri)) begin
+					tmp = nn1;
+					tmp2 = nn3;
+					pri = {1'b0,respbuf[(nn1+tndx)%CHANNELS][(nn3+t2ndx)%4].pri};
+					crr = 1'b1;
+				end
+			end
 		end
 	end
 end
@@ -138,10 +180,15 @@ always_ff @(posedge clk, posedge rst)
 if (rst) begin
 	resp_o <= 'd0;
 	for (nn2 = 0; nn2 < CHANNELS; nn2 = nn2 + 1) begin
+		chcnt[nn2] <= 'd0;
 		respbuf[nn2] <= 'd0;		
 	end
+	{tndx,t2ndx} <= 'd0;
 end
 else begin
+	tndx <= tndx + 2'd1;
+	if (tndx[HBIT-1:0]=='d0)
+		t2ndx <= t2ndx + 2'd1;
 	resp_o.ack <= 1'b0;
 	resp_o.err <= fta_bus_pkg::OKAY;
 	resp_o.rty <= 'd0;
@@ -150,21 +197,21 @@ else begin
 	resp_o.adr <= 'd0;
 	resp_o.stall <= 'd0;
 	resp_o.next <= 'd0;
-	if (!tmp[HBIT]) begin
-		respbuf[tmp[HBIT-1:0]].ack <= 1'b0;
+	if (crr) begin
+		respbuf[tmp2][tmp[HBIT-1:0]].ack <= 1'b0;
 		resp_o.ack <= 1'b1;
-		resp_o.err <= respbuf[tmp[HBIT-1:0]].err;
-		resp_o.rty <= respbuf[tmp[HBIT-1:0]].rty;
-		resp_o.dat <= respbuf[tmp[HBIT-1:0]].dat;
-		resp_o.cid <= respbuf[tmp[HBIT-1:0]].cid;
-		resp_o.tid <= respbuf[tmp[HBIT-1:0]].tid;
-		resp_o.adr <= respbuf[tmp[HBIT-1:0]].adr;
-		resp_o.pri <= respbuf[tmp[HBIT-1:0]].pri;
+		resp_o.err <= respbuf[tmp2][tmp[HBIT-1:0]].err;
+		resp_o.rty <= respbuf[tmp2][tmp[HBIT-1:0]].rty;
+		resp_o.dat <= respbuf[tmp2][tmp[HBIT-1:0]].dat;
+		resp_o.tid <= respbuf[tmp2][tmp[HBIT-1:0]].tid;
+		resp_o.adr <= respbuf[tmp2][tmp[HBIT-1:0]].adr;
+		resp_o.pri <= respbuf[tmp2][tmp[HBIT-1:0]].pri;
 	end
 	for (nn2 = 0; nn2 < CHANNELS; nn2 = nn2 + 1) begin
 		if (resp[nn2].ack) begin
-			respbuf[nn2] <= resp[nn2];
-			respbuf[nn2].ack <= 1'b1;
+			respbuf[chcnt[nn2]][nn2] <= resp[nn2];
+			respbuf[chcnt[nn2]][nn2].ack <= 1'b1;
+			chcnt[nn2] <= chcnt[nn2] + 1;
 		end
 	end
 end
@@ -181,22 +228,36 @@ output fta_cmd_response32_t resp_o;
 
 fta_cmd_response32_t [CHANNELS-1:0] respbuf [0:3];
 
-reg [HBIT:0] tmp;
-reg [1:0] tmp2;
+reg [4:0] pri;
+reg [HBIT:0] tmp, tndx;
+reg [1:0] tmp2, t2ndx;
 reg [1:0] chcnt [0:CHANNELS-1];
 
 integer nn1, nn2, nn3;
+integer crr;
+
+always_comb
+	if (CHANNELS != 8 && CHANNELS != 4 && CHANNELS != 2) begin
+		$display("fta_respbuf32: CHANNELS should be one of 2,4, or 8");
+		$finish;
+	end
 
 // Search for channel with response ready.
 always_comb
 begin
+	crr = 1'b0;
 	tmp2 = 'd0;
 	tmp = {HBIT+1{1'b1}};
+	pri = 5'h1F;
 	for (nn1 = 0; nn1 < CHANNELS; nn1 = nn1 + 1) begin
 		for (nn3 = 0; nn3 < 4; nn3 = nn3 + 1) begin
-			if (respbuf[nn3][nn1].ack && tmp[HBIT]) begin
-				tmp = nn1;
-				tmp2 = nn3;
+			if (respbuf[(nn1+tndx)%CHANNELS][(nn3+t2ndx)%4].ack) begin
+				if ($signed({1'b0,respbuf[(nn1+tndx)%CHANNELS][(nn3+t2ndx)%4].pri}) > $signed(pri)) begin
+					tmp = nn1;
+					tmp2 = nn3;
+					pri = {1'b0,respbuf[(nn1+tndx)%CHANNELS][(nn3+t2ndx)%4].pri};
+					crr = 1'b1;
+				end
 			end
 		end
 	end
@@ -209,25 +270,27 @@ if (rst) begin
 		chcnt[nn2] <= 'd0;
 		respbuf[nn2] <= 'd0;		
 	end
+	{tndx,t2ndx} <= 'd0;
 end
 else begin
+	tndx <= tndx + 2'd1;
+	if (tndx[HBIT-1:0]=='d0)
+		t2ndx <= t2ndx + 2'd1;
 	resp_o.ack <= 1'b0;
 	resp_o.err <= fta_bus_pkg::OKAY;
 	resp_o.rty <= 'd0;
 	resp_o.dat <= 'd0;
 	resp_o.tid <= 'd0;
-	resp_o.cid <= 'd0;
 	resp_o.adr <= 'd0;
 	resp_o.stall <= 'd0;
 	resp_o.next <= 'd0;
 	resp_o.pri <= 4'hF;
-	if (!tmp[HBIT]) begin
+	if (crr) begin
 		respbuf[tmp2][tmp[HBIT-1:0]].ack <= 1'b0;
 		resp_o.ack <= 1'b1;
 		resp_o.err <= respbuf[tmp2][tmp[HBIT-1:0]].err;
 		resp_o.rty <= respbuf[tmp2][tmp[HBIT-1:0]].rty;
 		resp_o.dat <= respbuf[tmp2][tmp[HBIT-1:0]].dat;
-		resp_o.cid <= respbuf[tmp2][tmp[HBIT-1:0]].cid;
 		resp_o.tid <= respbuf[tmp2][tmp[HBIT-1:0]].tid;
 		resp_o.adr <= respbuf[tmp2][tmp[HBIT-1:0]].adr;
 		resp_o.pri <= respbuf[tmp2][tmp[HBIT-1:0]].pri;
