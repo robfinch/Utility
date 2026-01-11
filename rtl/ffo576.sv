59f4bb106e0c11caa4acd7712505ff37fc285802
Robert Finch <5563401+robfinch@users.noreply.github.com>
2026-01-11 10:14:24 -0500

    Utility - update
    - find-x updates

---
+++ rtl/ffo576.sv
@@ -0,0 +1,60 @@
+// ============================================================================
+//        __
+//   \\__/ o\    (C) 2021-2026  Robert Finch, Waterloo
+//    \  __ /    All rights reserved.
+//     \/_//     robfinch<remove>@finitron.ca
+//       ||
+//
+//	ffo576.sv
+//
+// BSD 3-Clause License
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//                                                                          
+// ============================================================================
+
+module ffo576(i, o);
+input [575:0] i;
+output reg [9:0] o;
+
+wire [7:0] o1,o2,o3,o4;
+ffo144 u1 (i[575:432],o1);
+ffo144 u2 (i[431:288],o2);
+ffo144 u3 (i[287:144],o3);
+ffo144 u4 (i[143:  0],o4);
+always_comb
+if (o1==8'd255 && o2==8'd255 && o3==8'd255 && o4==8'd255)
+	o <= 10'd1023;
+else if (o1==8'd255 && o2==8'd255 && o3==8'd255)
+	o <= {2'b0,o4};
+else if (o1==8'd255 && o2==8'd255)
+	o <= 10'd144 + {2'b0,o3};
+else if (o1==8'd255)
+  o <= 10'd288 + {2'b0,o2};
+else
+  o <= 10'd432 + {2'b0,o1};
+
+endmodule
+
---
+++ rtl/flo288.sv
@@ -0,0 +1,54 @@
+// ============================================================================
+//        __
+//   \\__/ o\    (C) 2021-2026  Robert Finch, Waterloo
+//    \  __ /    All rights reserved.
+//     \/_//     robfinch<remove>@finitron.ca
+//       ||
+//
+//	flo288.sv
+//
+// BSD 3-Clause License
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//                                                                          
+// ============================================================================
+
+// Find last one
+module flo288(i, o);
+input [287:0] i;
+output reg [8:0] o;
+
+wire [7:0] o1,o2,o3,o4;
+flo144 u3 (i[287:144],o1);
+flo144 u4 (i[143:  0],o2);
+always_comb
+if (o1==8'd255 && o2==8'd255)
+	o <= 9'd511;
+else if (o1==8'd255)
+	o <= {2'b0,o2};
+else
+  o <= 9'd144 + {1'b0,o1};
+
+endmodule
